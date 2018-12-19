b17c2dba51afaa036d6a0c92d75418d397b2ec84
Meena Vempaty <meenamakesthings@gmail.com>
2018-12-18 18:29:09 -0800

    thrust baby thrust

--- unity/Assets/PlayerControl.cs
+++ unity/Assets/PlayerControl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    new private Rigidbody2D rigidbody;
+
+    [SerializeField]
+    private float speed = 0f;
+    [SerializeField]
+    private float rotateSpeed = 0f;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetButton("Jump")) { //Spacebar by default will make it move forward
+            rigidbody.AddRelativeForce(Vector2.up*speed);
+        }
     }
 }

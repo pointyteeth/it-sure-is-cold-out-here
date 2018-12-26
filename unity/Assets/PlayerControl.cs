a2d58fd1353e0cb5c9a3d6febfd7da8098025e39
Meena Vempaty <meenamakesthings@gmail.com>
2018-12-26 12:45:12 -0800

    ship movement

--- unity/Assets/PlayerControl.cs
+++ unity/Assets/PlayerControl.cs
@@ -6,21 +6,39 @@ public class PlayerControl : MonoBehaviour
 {
 
     new private Rigidbody2D rigidbody;
+    GameObject ship;
 
     [SerializeField]
     private float speed = 0f;
     [SerializeField]
-    private float rotateSpeed = 0f;
+    private float rotationSpeed = 0f;
+    [SerializeField]
+    private float shipRotationSpeed = 0f;
+    [SerializeField]
+    private float maxShipRotation = 0f;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        ship = GameObject.Find("Ship");
     }
 
     void FixedUpdate()
     {
-        if (Input.GetButton("Jump")) { //Spacebar by default will make it move forward
-            rigidbody.AddRelativeForce(Vector2.up*speed);
+        if (Input.GetButton("Jump")) { //Thrust
+            rigidbody.AddRelativeForce(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftShift)) { //Reverse Thrust
+            rigidbody.AddRelativeForce(Vector2.down * speed * Time.deltaTime);
+        }
+        float direction = -Input.GetAxis("Horizontal");
+        rigidbody.AddTorque(direction * rotationSpeed * Time.deltaTime); //Turn
+        float rotation = ship.transform.localRotation.eulerAngles.y;
+        if(rotation > 180) rotation -= 360;
+        if((direction < 0 && rotation > -maxShipRotation) || (direction > 0 && rotation < maxShipRotation)) { //Roll ship model
+            ship.transform.Rotate(Vector3.up * shipRotationSpeed * direction * Time.deltaTime);
+        } else if(direction == 0) {
+            ship.transform.localRotation = Quaternion.RotateTowards(ship.transform.localRotation, Quaternion.identity, Time.deltaTime * shipRotationSpeed);
         }
     }
 }
